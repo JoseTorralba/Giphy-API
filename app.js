b73173fe8e563f22ad0a9b8d699ca6ce601fd43d
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2019-03-12 03:21:42 -0400

    redone with sass, changed design, used fetch for api calls

--- app.js
+++ app.js
@@ -1,47 +1,82 @@
-// Animated Scroll
-$('#submit').click(function () {
-    $('html, body').animate({
-        scrollTop: $('#gifs').offset().top
-    }, 1000);
-});
+// Document on ready
+document.addEventListener("DOMContentLoaded", function() {
+    // Video Game Genre Object
+    const gameGenreObj = {
+        western: {
+            genre: 'Western Games',
+            icon: 'fas fa-horse-head'
+        },
+        fps: {
+            genre: 'First Person Games',
+            icon: 'fas fa-user'
+        },
+        rpg: {
+            genre: 'Role Playing Games',
+            icon: 'fas fa-dragon'
+        },
+        sports: {
+            genre: 'Sports Games',
+            icon: 'fas fa-futbol'
+        },
+        fighting: {
+            genre: 'Fighting Games',
+            icon: 'fas fa-fist-raised'
+        },
+        adventure: {
+            genre: 'Adventure Games',
+            icon: 'fas fa-map'
+        },
+        tpg: {
+            genre: 'Third Person Games',
+            icon: 'fas fa-walking'
+        },
+        racing: {
+            genre: 'Racing Games',
+            icon: 'fas fa-car'
+        },
+    };
 
-$(document).ready(function () {
 
-    // Array of movies genre
-    const movieGenre = [
-        "Western Games",
-        "First Person Games",
-        "Role Playing Games", 
-        "Thriller Games",
-        "Horror Games",
-        "Adventure Games",
-        "Third Person Games",
-        "Action Games"
-    ];
+    // Loops Through gameGenreObj
+    for (var key in gameGenreObj) {
+        if (gameGenreObj.hasOwnProperty(key)) {
 
-    // Empty String
-    let videoGameGenre = "";
+            // Gets ul Tag by Class
+            const buttonContainer = document.querySelector('.genre-showcase');
 
-    // Displays Gif Buttons
-    for (var i = 0; i < movieGenre.length; i++) {
+            // Creates List Items
+            const createListItem = document.createElement('li');
+            createListItem.classList.add('genre-showcase__list');
+            createListItem.innerHTML = `
+                <figure class="genre-showcase__fig">
+                    <button class="genre-showcase__btn" value="${gameGenreObj[key]['genre']}">${gameGenreObj[key]['genre']}
+                        <i class="${gameGenreObj[key]['icon']} genre-showcase__btn--icon"></i> 
+                    </button>
+                </figure>
+            `;
+            // Appends Create List Item
+            buttonContainer.appendChild(createListItem);
+        }
+    };
 
-        const gifBtn = document.getElementById('buttonDiv');
-        const createButton = document.createElement('button');
-        createButton.classList.add("movie-genre");
 
-        createButton.innerHTML = movieGenre[i];
-        createButton.value = movieGenre[i];
+    // Gets Genre Button by Class
+    var genreButton = document.querySelectorAll('.genre-showcase__btn');
 
-        gifBtn.appendChild(createButton);
-    }
+    // Genre Button on Click
+    genreButton.forEach(function (genreButton) {
+        genreButton.addEventListener('click', function() {
+            // Gets Value of Genre Clicked
+            videoGameGenre = this.value; 
+        });
+	});
 
-    $('.movie-genre').on('click', function () {
-        videoGameGenre = this.value;
-    });
 
-    // Shows Gif results when sumbit button is clicked
-    $('#submit').click(function (event) {
+    // Gets Submit Button by Class
+    var submitButton = document.querySelector('.submit-btn');
 
+    // Submit Button on Click
+    submitButton.addEventListener('click', function() {
         var offset = Math.round(Math.random() * 100);
         var query = videoGameGenre;
         var key = "gc7kXsvMsDTr6mMHxbc8FMK8D23EPLG2";
@@ -53,27 +88,43 @@ $(document).ready(function () {
             + "&offset="
             + offset;
 
-        $.getJSON(url, function (json) {
-            console.log(json);
 
-            // Hides previous results
-            document.getElementById('gifs').innerHTML = "";
+        // Fetches Giphy API Data
+        fetch(url).then(response => {
+            return response.json();
+        }).then(giphy => {
 
-            // Displays Gifs
-            for (let i = 0; i < json.data.length; i++) {
+            // Gets Gif Container Div and Hides previous Gif results
+            const getGifContainer = document.querySelector('.gif-container');
+            getGifContainer.innerHTML = "";
 
-                const gif = json.data[i];
+            for (let i = 0; i < giphy.data.length; i++) {
+                const gif = giphy.data[i];
 
-                const imgElem = $('<img>')
-                    .attr('src', gif.images.downsized.url)
+                // Creates Div Containing Gif
+                const imgContainer = document.createElement('div');
+                imgContainer.classList.add('gif-container__gif');
 
-                const imgContainer = $('<div>')
-                    .addClass('gif');
+                // Creates Img to Display Gif
+                const createImg = document.createElement('img');
+                createImg.setAttribute('src', gif.images.downsized.url);
+                createImg.classList.add('gif-container__gif--img')
 
-                imgContainer.append(imgElem)
+                // Display Gifs
+                imgContainer.append(createImg)
+                getGifContainer.append(imgContainer);
 
-                $('#gifs').append(imgContainer);
             };
+        }).catch(err => {
+            console.log('There is and Error of ' + err);
         });
-    }); 
+    });
+
+
+    // Animated Scroll
+    $('.submit-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#gifs').offset().top
+        }, 1000);
+    });
 });
